b161d23f50bb47e7168a3f4b47840ed36ba8b988
RuoShui-0014 <ruoshui0014@outlook.com>
2024-07-16 18:04:28 +0800

    Index code commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const Tool = require(`${__dirname}/Tools/ToolCode.js`);
+
+const WebSocket = require('ws');
+
+const VMCode = Tool.GetVMCode(0);
+const ivm = require(`${__dirname}/Tools/isolated-rsvm.node`).ivm;
+
+let code = VMCode + fs.readFileSync(`${__dirname}/Sites/rs6_药监局.js`);
+
+let isolate = new ivm.Isolate({ inspector: true, memoryLimit: 1024 });
+(async function () {
+    context = await isolate.createContext({ inspector: true, rsvm: true, intercept: true });
+
+    debugger
+    let script = await isolate.compileScriptSync(code, { filename: 'example.js' });
+    let result = await script.run(global.context);
+    console.log(result);
+}()).catch(console.error);
+
+const PORT = 9229;
+let wss = new WebSocket.Server({ port: PORT });
+wss.on('connection', function (ws) {
+    // Dispose inspector session on websocket disconnect
+    let channel = isolate.createInspectorSession();
+    function dispose() {
+        try {
+            channel.dispose();
+        } catch (err) { }
+    }
+    ws.on('error', dispose);
+    ws.on('close', dispose);
+
+    // Relay messages from frontend to backend
+    ws.on('message', function (message) {
+        try {
+            channel.dispatchProtocolMessage(String(message));
+        } catch (err) {
+            // This happens if inspector session was closed unexpectedly
+            ws.close();
+        }
+    });
+
+    // Relay messages from backend to frontend
+    function send(message) {
+        try {
+            ws.send(message);
+        } catch (err) {
+            dispose();
+        }
+    }
+    channel.onResponse = (callId, message) => send(message);
+    channel.onNotification = send;
+});
+console.log('devtools://devtools/bundled/inspector.html?experiments=true&v8only=true&ws=127.0.0.1:' + PORT);
